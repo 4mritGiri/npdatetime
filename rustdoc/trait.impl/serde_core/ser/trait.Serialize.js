02686b331c4b16d444d27839add8ee71fc22bfce
4mritGiri <4mritGiri@users.noreply.github.com>
2026-05-26 10:39:31 +0000

    deploy: 5066fd826e8cccd5ef6a3c3338e233d00fd53c1d

--- rustdoc/trait.impl/serde_core/ser/trait.Serialize.js
+++ rustdoc/trait.impl/serde_core/ser/trait.Serialize.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["npdatetime",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"npdatetime/astronomical/calendar/bs_date/struct.BsDate.html\" title=\"struct npdatetime::astronomical::calendar::bs_date::BsDate\">BsDate</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"npdatetime/core/date/struct.NepaliDate.html\" title=\"struct npdatetime::core::date::NepaliDate\">NepaliDate</a>"]]]]);
+    const implementors = Object.fromEntries([["npdatetime",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"npdatetime/astronomical/calendar/bs_date/struct.BsDate.html\" title=\"struct npdatetime::astronomical::calendar::bs_date::BsDate\">BsDate</a>",0],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"npdatetime/core/date/struct.NepaliDate.html\" title=\"struct npdatetime::core::date::NepaliDate\">NepaliDate</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[663]}
+//{"start":59,"fragment_lengths":[667]}
